a38c545691636873e31428e8d54ffe20884de6b5
Matt McDowall <51373511+MattMcDowall@users.noreply.github.com>
2022-08-12 20:59:24 -0500

    Create sync_test.js

---
+++ sync_test.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         🙈 Sync Test
+// @version      1.0
+// @author       spoko
+// @match        *://*/*
+// @downloadURL  https://github.com/MattMcDowall/TM_Scripts/raw/main/sync_test.js
+// @grant        none
+// ==/UserScript==
+
